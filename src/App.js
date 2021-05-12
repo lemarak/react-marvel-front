ccfaf2d6b06307484a4bcba57db7291f287202a0
Stéphane Dauguet <sdauguet@gmail.com>
2021-05-12 16:34:18 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div>
-      Hello from <a href="https://www.lereacteur.io">Le Reacteur !</a>
-    </div>
-  );
+  return <div>Welcome to API Marvel</div>;
 }
 
 export default App;
